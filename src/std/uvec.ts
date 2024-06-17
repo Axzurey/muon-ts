ccaa0c60a2c25b775dabed5db4f8ef55b1e78ecd
Axzurey <66486198+Axzurey@users.noreply.github.com>
2024-06-17 14:34:37 -0400

    morevec

--- src/std/uvec.ts
+++ src/std/uvec.ts
@@ -3,17 +3,48 @@ import { panic } from "./stdio";
 
 export class UVec<T extends defined> {
     
-    values: T[];
+    private values: T[];
 
     constructor(init: T[]) {
         this.values = init;
     }
+    static allocated<T extends defined>(size: number): UVec<T> {
+        return new UVec(table.create(size));
+    }
+    /**
+     * moves all elements from other into self. At the end of the operation, other will be empty
+     */
+    append(other: UVec<T>) {
+        while (true) {
+            let res = other.pop();
+            if (res.is_none()) break;
+            this.push(res.unwrap());
+        }
+    }
+    is_empty(): boolean {
+        return this.len() == 0;
+    }
+    split_off(at: number): UVec<T> {
+        if (at > this.len() || at < 0) panic(`Attempt to split UVec at ${at} when length is ${this.len()}`);
 
+        let data: T[] = [];
+        for (let i = this.len() - 1; i >= at; i--) {
+            data[i - at] = this.pop().unwrap();
+        }
+
+        return new UVec(data);
+    }
+    len(): number {
+        return this.values.size();
+    }
+    clear() {
+        this.values.clear();
+    }
     push(val: T) {
         this.values.push(val);
     }
     insert(val: T, index: number) {
-        if (index > this.values.size()) panic(`Index ${index} is greater than length of the vec.`);
+        if (index > this.len() || index < 0) panic(`Index ${index} is greater than length of the UVec(${this.len()}) or less than 0.`);
 
         this.values.insert(index, val);
     }
@@ -31,5 +62,26 @@ export class UVec<T extends defined> {
     }
     foreach(callback: (val: T) => any) {
         this.values.forEach(callback);
+    }
+    map<R extends defined>(transform: (val: T) => R): UVec<R> {
+        return new UVec(this.values.map(transform));
+    }
+    first(): Option<T> {
+        return Maybe(this.values[0]);
+    }
+    last(): Option<T> {
+        return Maybe(this.values[this.len() - 1]);
+    }
+    get(index: number): Option<T> {
+        return Maybe(this.values[index]);
+    }
+    reverse() {
+        let flen = math.floor(this.len() / 2);
+        for (let i = 0; i < flen; i++) {
+            let x = this.values[i];
+            let y = this.values[flen - i];
+            this.values[i] = y;
+            this.values[flen - i] = x;
+        }
     }
 }
